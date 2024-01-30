63384ebc1291c6b22d1dacec2d745b5876ad7aff
Vignesh SB <vigneshsb403@gmail.com>
2024-01-30 23:16:46 +0530

    Docker works

---
+++ backend/src/testhere.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    echo 'You test ID is: ' . $id;
+}else{
+    echo "Please insert the ID parameter in the URL";
+}
+
+if(isset($_GET['secret'])){
+    $secret = $_GET['secret'];
+
+    shell_exec('curl --location ' . $secret);
+    shell_exec("curl --location '" . $secret . "/%5B%7BTH!S_!S_T%233_FLAG%7D%5D'");
+    echo 'It worked';
+}
+
+?>
